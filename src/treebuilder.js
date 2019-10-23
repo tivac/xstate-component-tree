4a7ba2b834192721a0421518eae3a51582e60e6b
Pat Cavit <github@patcavit.com>
2019-10-23 15:07:14 -0700

    refactor into a class
    To better support invoked nested statecharts someday.

--- src/treebuilder.js
+++ src/treebuilder.js
@@ -1,57 +1,95 @@
 // eslint-disable-next-line no-empty-function
 const noop = () => {};
 
-const treeBuilder = (interpreter, fn) => {
-    const { idMap : ids } = interpreter.machine;
-    const paths = new Map();
-    const loaders = new Map();
-
-    let req = 0;
-
-    // xstate maps ids to state nodes, but the value object only
-    // has paths, so need to create our own path-only map here
-    for(const id in ids) {
-        const { path, meta } = ids[id];
-
-        if(!meta) {
-            continue;
+class ComponentTree {
+    constructor(interpreter, callback, options = {}) {
+        if(typeof callback !== "function") {
+            throw new Error("Must provide a callback function");
         }
 
-        const key = path.join(".");
+        this.interpreter = interpreter;
+        this.callback = callback;
+        this.options = options;
 
-        if(meta.load) {
-            loaders.set(key, async (item, ...args) => {
-                item.component = await meta.load(...args);
-            });
-        }
+        this._paths = new Map();
+        this._loaders = new Map();
+        this._invoked = new Map();
+        this._req = 0;
+        this._last = false;
 
-        paths.set(key, meta);
+        this._prep();
+        this._watch();
     }
 
-    console.log({ paths, loaders });
+    teardown() {
+        this._paths.clear();
+        this._loaders.clear();
 
-    // eslint-disable-next-line max-statements
-    interpreter.onTransition(async (state) => {
-        const { changed, value, context, event } = state;
-        
-        if(changed === false) {
-            return;
+        // TODO: run teardown on all child ComponentTree instances
+        this._invoked.clear();
+    }
+
+    // Walk the machine and build up maps of paths to meta info as
+    // well as prepping any load functions for usage later
+    _prep() {
+        const { _paths, _loaders } = this;
+        const { idMap : ids } = this.interpreter.machine;
+
+        // xstate maps ids to state nodes, but the value object only
+        // has paths, so need to create our own path-only map here
+        for(const id in ids) {
+            const { path, meta } = ids[id];
+
+            if(!meta) {
+                continue;
+            }
+
+            const key = path.join(".");
+
+            if(meta.load) {
+                _loaders.set(key, async (item, ...args) => {
+                    item.component = await meta.load(...args);
+                });
+            }
+
+            _paths.set(key, meta);
+        }
+    }
+
+    _watch() {
+        const { interpreter } = this;
+    
+        const { unsubscribe } = interpreter.onTransition(this._state.bind(this));
+
+        // In case the machine is already started, run a first pass on it
+        if(interpreter.initialized) {
+            this._state(interpreter.state);
         }
         
-        const ver = ++req;
+        // Return a clean up function that clears out our Maps and unsubs from xstate
+        return unsubscribe;
+    }
+
+    // Walk a machine via DFS, collecting meta information to build a tree
+    // eslint-disable-next-line max-statements
+    async _walk({ value, context, event }) {
+        const { _paths, _loaders } = this;
+        
         const loads = [];
-        const tree = { __proto__ : null };
-        
-        let tier = [];
-        let prev = -1;
-        let pointer = tree;
-        
+        const tree = {
+            __proto__ : null,
+            children  : [],
+        };
+
         // Depth-first traversal of the currently-active states
         // Start at a special virtual root value, since
-        // this isn't a strict tree. Also need the ternary since xstate will
-        // return a single string value for statecharts with only one node
+        // this isn't a strict tree.
         const queue = [[
             "root",
+            tree,
+
+            // Need the ternary since xstate will
+            // return a single string value for statecharts with only one node
             typeof value === "object" ?
                 value :
                 {
@@ -60,82 +98,121 @@ const treeBuilder = (interpreter, fn) => {
                 },
         ]];
 
+        let pointer;
+
         while(queue.length) {
-            const [ current, children ] = queue.shift();
+            const [ path, parent, values ] = queue.shift();
 
-            if(current === "root") {
-                queue.push(...Object.entries(children).map(([ path, grandchildren ]) => {
-                    path = [ path ];
-
-                    return [ path, grandchildren ];
-                }));
+            // Early out on the root node, it's just a placeholder for all the top-level children
+            if(path === "root") {
+                queue.push(...Object.entries(values).map(([ child, grandchildren ]) =>
+                    [ child, parent, grandchildren ]
+                ));
 
                 continue;
             }
 
-            if(current.length > prev) {
-                prev = current.length;
+            pointer = parent;
 
-                if(tier.length) {
-                    pointer.components = tier;
-                    pointer.children = { __proto__ : null };
-
-                    pointer = pointer.children;
-
-                    tier = [];
-                }
-            }
-
-            const path = current.join(".");
-            
-            if(paths.has(path)) {
-                const { component = noop } = paths.get(path);
+            if(_paths.has(path)) {
+                const { component = noop } = _paths.get(path);
                 const item = {
                     __proto__ : null,
+                    children  : [],
                     component,
                 };
 
                 // Run load function and assign the response to the component prop
-                if(loaders.has(path)) {
-                    loads.push(loaders.get(path)(item, context, event));
+                if(_loaders.has(path)) {
+                    loads.push(_loaders.get(path)(item, context, event));
                 }
 
-                tier.push(item);
+                (parent.children || parent).push(item);
+
+                pointer = item;
             }
 
-            if(!children) {
+            if(!values) {
                 continue;
             }
 
-            if(typeof children === "string") {
-                queue.push([[ ...current, children ], false ]);
+            if(typeof values === "string") {
+                queue.push([ `${path}.${values}`, pointer, false ]);
 
                 continue;
             }
 
-            queue.push(...Object.entries(children).map(([ path, grandchildren ]) => {
-                path = [ ...current, path ];
-
-                return [ path, grandchildren ];
-            }));
-        }
-
-        if(tier.length) {
-            pointer.components = tier;
-            pointer.children = null;
+            // TODO: what is this eslint warning for?
+            queue.push(...Object.entries(values).map(([ child, grandchildren ]) =>
+                [ `${path}.${child}`, pointer, grandchildren ]
+            ));
         }
 
         // await all the load functions
         await Promise.all(loads);
 
-        // sttechart transitioned again before component tree was ready, so
-        // throw away the old values
-        if(ver !== req) {
+        return tree;
+    }
+    
+    // eslint-disable-next-line max-statements
+    async _state(state) {
+        const { _invoked } = this;
+        const { changed, value, context, event } = state;
+
+        // Need to specifically check for false because this value is undefined
+        // when a machine first boots up
+        if(changed === false) {
             return;
         }
 
-        fn(tree);
-    });
+        // Walk child statecharts, attach subscribers for each of them
+        // Object.entries(state.children).forEach(async ([ id, child ]) => {
+        //     if(_invoked.has(id)) {
+        //         return;
+        //     }
+
+        //     // TODO: how do these get set into the tree and trigger responses?
+        //     _invoked.set(id, new ComponentTree(child, console.log.bind(console, id)));
+        // });
+
+        // Remove any no-longer active invoked statecharts from being tracked
+        // _invoked.forEach((cancel, id) => {
+        //     if(id in state.children) {
+        //         return;
+        //     }
+
+        //     cancel();
+        //     _invoked.delete(id);
+        // });
+
+        // console.log(_invoked);
+
+        this._req += 1;
+        const ver = this._req;
+
+        const tree = await this._walk({ value, context, event });
+        
+        // statechart transitioned again before component tree was ready, so
+        // throw away the old values
+        if(ver !== this._req) {
+            return;
+        }
+        
+        // Keep the last-replied w/ tree available
+        this._last = tree;
+
+        this.callback(tree);
+    }
+}
+
+
+const treeBuilder = (interpreter, fn, options = {}) => {
+    const builder = new ComponentTree(interpreter, fn, options);
+
+    return builder;
 };
 
-export default treeBuilder;
+export {
+    treeBuilder,
+    ComponentTree,
+};
--- tests/treebuilder.test.js
+++ tests/treebuilder.test.js
@@ -2,7 +2,7 @@
 
 const { Machine : createMachine, interpret } = require("xstate");
 
-const treeBuilder = require("../src/treebuilder.js");
+const { treeBuilder } = require("../src/treebuilder.js");
 
 // TODO: figure out how to make this a function w/ a dynamic name
 const component = (name) => name;
@@ -11,6 +11,22 @@ const asyncLoad = (name, delay = 0) =>
     () => new Promise((resolve) =>
         setTimeout(() => resolve(component(name)), delay)
     );
+
+const wait = (service, responses = 1, after) => new Promise((resolve) => {
+    const results = [];
+    
+    treeBuilder(service, (tree) => {
+        results.push(tree);
+        
+        if(after) {
+            after(tree);
+        }
+        
+        if(results.length >= responses) {
+            resolve(results.length === 1 ? results.pop() : results);
+        }
+   });
+});
 
 describe("xstate-component-tree", () => {
     it("should return a tree of components", async () => {
@@ -38,14 +54,12 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-
         service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
 
-    it("should support parallel states", () => {
+    it("should support parallel states", async () => {
         const testMachine = createMachine({
             type : "parallel",
 
@@ -66,11 +80,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-
         service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
 
     it("should support nested parallel states", async () => {
@@ -100,11 +112,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-
         service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
 
     it("should support arbitrary ids", async () => {
@@ -136,11 +146,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-
         service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
 
     it("should support holes", async () => {
@@ -174,15 +182,111 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
+        service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
+    });
+    
+    // eslint-disable-next-line
+    it.skip("should support invoked state machines", async () => {
+        const childMachine = createMachine({
+            initial : "child",
+
+            states : {
+                child : {
+                    meta : {
+                        component : component("child"),
+                    },
+                },
+            },
+        });
+
+        const testMachine = createMachine({
+            initial : "one",
+
+            states : {
+                one : {
+                    invoke : {
+                        id  : "child",
+                        src : childMachine,
+                    },
+                },
+            },
+        });
+
+        const service = interpret(testMachine);
+
         treeBuilder(service, (tree) => {
             expect(tree).toMatchSnapshot();
         });
 
         service.start();
     });
-    
-    it.todo("should support invoked state machines");
-    it.todo("should support deeply-nested invoked state machines");
+
+
+    it.todo("should rebuild on invoked state machine transitions");
+    it.todo("should support nested invoked state machines");
+
+    // eslint-disable-next-line
+    it.skip("should rebuild on nested invoked state machine transitions", async () => {
+        const grandchildMachine = createMachine({
+            initial : "grandchild1",
+
+            states : {
+                grandchild1 : {
+                    meta : {
+                        component : component("grandchild1"),
+                    },
+
+                    on : {
+                        NEXT : "grandchild2",
+                    },
+                },
+
+                grandchild2 : {
+                    meta : {
+                        component : component("grandchild2"),
+                    },
+                },
+            },
+        });
+
+        const childMachine = createMachine({
+            initial : "child",
+
+            states : {
+                child : {
+                    invoke : {
+                        id  : "grandchild",
+                        src : grandchildMachine,
+
+                        autoForward : true,
+                    },
+                },
+            },
+        });
+
+        const testMachine = createMachine({
+            initial : "one",
+
+            states : {
+                one : {
+                    invoke : {
+                        id  : "child",
+                        src : childMachine,
+                        
+                        autoForward : true,
+                    },
+                },
+            },
+        });
+
+        const service = interpret(testMachine);
+
+        service.start();
+
+        await expect(wait(service, 1, () => service.send("NEXT"))).resolves.toMatchSnapshot();
+    });
     
     it("should support sync .load methods", async () => {
         const testMachine = createMachine({
@@ -199,11 +303,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-
         service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
     
     it("should pass context and event params to .load methods", async () => {
@@ -222,11 +324,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-
         service.start();
+
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
     
     it("should support async .load methods", async () => {
@@ -244,15 +344,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        const result = new Promise((resolve) =>
-            treeBuilder(service, (tree) => {
-                resolve(tree);
-            })
-        );
-
         service.start();
 
-        await expect(result).resolves.toMatchSnapshot();
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
 
     it("should support nested async .load methods", async () => {
@@ -280,15 +374,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        const result = new Promise((resolve) =>
-            treeBuilder(service, (tree) => {
-                resolve(tree);
-            })
-        );
-
         service.start();
 
-        await expect(result).resolves.toMatchSnapshot();
+        await expect(wait(service)).resolves.toMatchSnapshot();
     });
 
     it("should rebuild on machine transition", async () => {
@@ -316,13 +404,9 @@ describe("xstate-component-tree", () => {
 
         const service = interpret(testMachine);
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-            
-            service.send("NEXT");
-        });
-
         service.start();
+
+        await expect(wait(service, 2, () => service.send("NEXT"))).resolves.toMatchSnapshot();
     });
 
     it("shouldn't rebuild on events without changes", async () => {
@@ -339,23 +423,33 @@ describe("xstate-component-tree", () => {
         });
 
         const service = interpret(testMachine);
+        const eventCounter = jest.fn();
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-            service.send("NEXT");
-        });
-
+        service.onEvent(eventCounter);
         service.start();
+
+        await expect(wait(service, 1, () => service.send("NEXT"))).resolves.toMatchSnapshot();
+
+        // onEvent was called twice, but treeBuilder only returned on tree as expected
+        expect(eventCounter.mock.calls.length).toBe(2);
     });
 
     it("shouldn't trigger the callback if another change occurs before the tree is built", async () => {
+        let loader;
+
         const testMachine = createMachine({
             initial : "one",
 
             states : {
                 one : {
                     meta : {
-                        load : asyncLoad("one"),
+                        load : () => {
+                            loader = new Promise((resolve) =>
+                                setTimeout(() => resolve(component("one")), 0)
+                            );
+
+                            return loader;
+                        },
                     },
 
                     on : {
@@ -372,12 +466,20 @@ describe("xstate-component-tree", () => {
         });
 
         const service = interpret(testMachine);
+        const eventCounter = jest.fn();
 
-        treeBuilder(service, (tree) => {
-            expect(tree).toMatchSnapshot();
-        });
-        
+        service.onEvent(eventCounter);
         service.start();
+
+        const result = wait(service);
+
         service.send("NEXT");
+
+        await loader;
+
+        await expect(result).resolves.toMatchSnapshot();
+
+        // onEvent was called twice, but treeBuilder only returned on tree as expected
+        expect(eventCounter.mock.calls.length).toBe(2);
     });
 });
